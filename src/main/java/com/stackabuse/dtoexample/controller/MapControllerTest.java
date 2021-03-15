f6c7f519c377b7c0b04b756bb8a3fd43891355a7
Ronnen <nagalr@gmail.com>
2021-03-15 13:02:55 +0200

    Fix dependency conflicts in pom.xml, add a test class to MapController class

---
+++ src/main/java/com/stackabuse/dtoexample/controller/MapControllerTest.java
@@ -0,0 +1,40 @@
+package com.stackabuse.dtoexample.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+/**
+ * Created by ronnen on 15-Mar-2021
+ */
+
+@ExtendWith(MockitoExtension.class)
+class MapControllerTest {
+
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllUsersLocation() {
+    }
+
+    @Test
+    void getAllUsersLocation_Mapper() {
+    }
+
+    @Test
+    void getUserLocation() {
+    }
+
+    @Test
+    void addLocation() {
+    }
+
+    @Test
+    void getAllLocations() {
+    }
+}
